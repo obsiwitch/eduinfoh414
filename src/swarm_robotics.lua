eb1a2a8281d3c92537371e50637a6169cd37dee9
Obside <obsiagae@gmail.com>
2015-05-22 08:53:11 +0200

    robot type

--- src/swarm_robotics.lua
+++ src/swarm_robotics.lua
@@ -3,10 +3,47 @@ G_ROBOT_COLOR = "yellow"
 L_ROBOT_COLOR = "cyan"
 
 --[[
+ Current robot's type (G or L).
+ Initialized to U (unknown) and then set to its correct value in the init()
+ function.
+--]]
+robotType = "U"
+
+--[[
+ Gets the current robot's type.
+ * type G: ground sensor
+ * type L: light sensor
+ * type U: unknown
+--]]
+function getRobotType()
+    if (robot.motor_ground ~= nil) then
+        return "G"
+    elseif (robot.light ~= nil) then
+        return "L"
+    else
+        return "U"
+    end
+end
+
+--[[
+ Sets the robot's color depending on its type.
+ * type G: yellow
+ * type L: cyan
+--]]
+function setRobotColor(robotType)
+	if (robotType == "G") then
+		robot.leds.set_all_colors(G_ROBOT_COLOR)
+	elseif (robotType == "L") then
+		robot.leds.set_all_colors(L_ROBOT_COLOR)
+	end
+end
+
+--[[
  This function is executed every time you press the 'execute' button.
 --]]
 function init()
-   -- TODO
+	robotType = getRobotType()
+	setRobotColor(robotType)
 end
 
 --[[
@@ -24,7 +61,7 @@ end
  automatically by ARGoS.
 --]]
 function reset()
-   -- TODO
+   init()
 end
 
 --[[
