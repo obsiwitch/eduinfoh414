1950406988cbadd9797425d6db12f8dda3dd7963
Obside <obsiagae@gmail.com>
2015-05-22 06:43:28 +0200

    lua script template

---
+++ src/swarm_robotics.lua
@@ -0,0 +1,32 @@
+--[[
+ This function is executed every time you press the 'execute' button.
+--]]
+function init()
+   -- TODO
+end
+
+--[[
+ This function is executed at each time step. It must contain the logic of your
+ controller
+--]]
+function step()
+    -- TODO
+end
+
+--[[
+ This function is executed every time you press the 'reset' button in the GUI.
+ It is supposed to restore the state of the controller to whatever it was right
+ after init() was called. The state of sensors and actuators is reset
+ automatically by ARGoS.
+--]]
+function reset()
+   -- TODO
+end
+
+--[[
+ This function is executed only once, when the robot is removed from the
+ simulation.
+--]]
+function destroy()
+   -- TODO
+end
