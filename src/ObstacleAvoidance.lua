7aed2d4acc4b80165f53a388398ece728a5ac00a
Obside <obsiagae@gmail.com>
2015-05-24 20:10:20 +0200

    Improved obstacle avoidance
    The computeSpeedsFromAngle() function now allows us to move accurately in the
    direction specified by an angle.

--- src/ObstacleAvoidance.lua
+++ src/ObstacleAvoidance.lua
@@ -1,7 +1,17 @@
 --[[
  This file holds a step function allowing to do obstacle avoidance. The behavior
- is the one seen in class (1st course exercise, obstacleAvoidance_vec).
+ is the one seen in class, slightly modified (1st course exercise,
+ obstacleAvoidance_vec).
 --]]
+
+-- Max wheel speed
+local WHEEL_SPEED = 10
+
+-- Coefficient for computing the angular velocity
+local K_PROP = 20;
+
+-- Distance between the 2 wheels (in meters)
+local WHEELS_DISTANCE = 0.14;
 
 --[[
  Converts cylindrical coordinates to cartesian coordinates.
@@ -46,30 +56,63 @@ function getObstacleVector(proximityTable)
 end
 
 --[[
+ Computes the speeds which need to be applied to both wheels in order to go in
+ the direction of the specified angle.
+ 
+ @note this function was seen during the 4th course exercise (pattern_formation)
+--]]
+function ComputeSpeedsFromAngle(angle)
+    local forwardMotionCoeff
+    local targetIsBehind = (angle > math.pi/2 or angle < -math.pi/2)
+    
+    -- Compute the forward motion coeffcieint ([0,1]) from the angle. If the
+    -- target is behind the robot, we just rotate (no forward motion).
+    if targetIsBehind then
+        forwardMotionCoeff = 0.0;
+    else
+        forwardMotionCoeff = math.cos(angle)
+    end
+
+	 -- The angular velocity component is the desired angle scaled linearly.
+    local angularVelocity = K_PROP * angle;
+    
+    return {
+        forwardMotionCoeff * WHEEL_SPEED - angularVelocity * WHEELS_DISTANCE,
+        forwardMotionCoeff * WHEEL_SPEED + angularVelocity * WHEELS_DISTANCE
+    }
+end
+
+--[[
+ Compute the opposite vector from a vector in cylindrical coordinates.
+--]]
+function ComputeOppositeVector(cylindricalVector)
+    local oppositeAngle
+    if (cylindricalVector.angle > 0) then
+        oppositeAngle = cylindricalVector.angle - math.pi
+    else
+        oppositeAngle = cylindricalVector.angle + math.pi
+    end
+    
+    return {
+        value = -cylindricalVector.value,
+        angle = oppositeAngle
+    }
+end
+
+--[[
  Use the proximity sensors to avoid obstacles. An obstacle vector is obtained
- by reading all the values from the sensors. We can then determine whether
- there's an obstacle, and whether the robot should turn right or left if it is
- the case. If no obstacle is detected, simply go straight.
- The robot turns with a speed that depends on the computed angle. The closer
- the obstacle is to the x axis of the robot, the quicker the turn.
+ by reading all the values from the sensors. We can then avoid obstacles by
+ going in the direction of the opposite vector.
 --]]
 function stepAvoid()
     local obstacleVector = getObstacleVector(robot.proximity)
+    local escapeVector = ComputeOppositeVector(obstacleVector)
     local obstacleDetected = (obstacleVector.value > 0.2)
 
     if obstacleDetected then
-        -- Velocity which will be applied to the wheel corresponding to the
-        -- side where the obstacle is.
-        local velocity = math.max(0.5, math.cos(obstacleVector.angle)) * 10
-        
-        if obstacleVector.angle > 0 then
-            -- obstacle on the left
-            robot.wheels.set_velocity(velocity, 0)
-        else
-            -- obstacle on the right
-            robot.wheels.set_velocity(0, velocity)
-        end
+        local speeds = ComputeSpeedsFromAngle(escapeVector.angle)
+        robot.wheels.set_velocity(speeds[1], speeds[2])
     else
-        robot.wheels.set_velocity(10,10)
+        robot.wheels.set_velocity(WHEEL_SPEED, WHEEL_SPEED)
     end
 end
