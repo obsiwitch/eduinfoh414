9c82394af72c8a924cdafcc64fc81df0463df1d6
Obside <obsiagae@gmail.com>
2015-05-24 20:20:05 +0200

    Movement
    Move general movement related functions into a new file (splitted from
    ObstacleAvoidance.lua).

--- src/ObstacleAvoidance.lua
+++ src/ObstacleAvoidance.lua
@@ -1,17 +1,10 @@
+require "src/Movement"
+
 --[[
- This file holds a step function allowing to do obstacle avoidance. The behavior
- is the one seen in class, slightly modified (1st course exercise,
+ This file holds functions related to obstacle avoidance. The behaviour is the
+ one seen in class, slightly modified (1st course exercise,
  obstacleAvoidance_vec).
 --]]
-
--- Max wheel speed
-local WHEEL_SPEED = 10
-
--- Coefficient for computing the angular velocity
-local K_PROP = 20;
-
--- Distance between the 2 wheels (in meters)
-local WHEELS_DISTANCE = 0.14;
 
 --[[
  Converts cylindrical coordinates to cartesian coordinates.
@@ -53,33 +46,6 @@ function getObstacleVector(proximityTable)
     local obstacleVector = CartesianTocylindricalCoords(accumulator)
     
     return obstacleVector
-end
-
---[[
- Computes the speeds which need to be applied to both wheels in order to go in
- the direction of the specified angle.
- 
- @note this function was seen during the 4th course exercise (pattern_formation)
---]]
-function ComputeSpeedsFromAngle(angle)
-    local forwardMotionCoeff
-    local targetIsBehind = (angle > math.pi/2 or angle < -math.pi/2)
-    
-    -- Compute the forward motion coeffcieint ([0,1]) from the angle. If the
-    -- target is behind the robot, we just rotate (no forward motion).
-    if targetIsBehind then
-        forwardMotionCoeff = 0.0;
-    else
-        forwardMotionCoeff = math.cos(angle)
-    end
-
-	 -- The angular velocity component is the desired angle scaled linearly.
-    local angularVelocity = K_PROP * angle;
-    
-    return {
-        forwardMotionCoeff * WHEEL_SPEED - angularVelocity * WHEELS_DISTANCE,
-        forwardMotionCoeff * WHEEL_SPEED + angularVelocity * WHEELS_DISTANCE
-    }
 end
 
 --[[
