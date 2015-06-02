1c49c25d77e2fe960e8f652f51c19ce0bf0ee978
Obside <obsiagae@gmail.com>
2015-06-02 23:32:52 +0200

    MoveIntoRoom update doc

--- src/MoveIntoRoom.lua
+++ src/MoveIntoRoom.lua
@@ -52,10 +52,7 @@ function MoveIntoRoom.init(doorColor)
     --[[ public methods ]]
     
     --[[
-     Step function to move a robot to the specified room.
-     1) attraction by door
-     2) after threshold attained, move forward
-     3) after a 2nd threshold is attained, we consider the robot is in the room
+     Step function to move the current robot to the specified room.
      
      Returns true once the robot is inside the room, else returns false.
     --]]
@@ -69,13 +66,16 @@ function MoveIntoRoom.init(doorColor)
             return false
         end
         
+        -- 1) Attraction by door.
         if (state == STATES.DOOR_ATTRACTION) then
             Bot.goTowardsAngle(targetDoor.angle)
             
             if (targetDoor.distance < DOOR_THRESHOLD) then
                 state = STATES.MOVE_INTO_ROOM
             end
-            
+        
+        -- 2) First threshold attained, go inside the room by attracting the
+        -- robot to various elements inside.
         elseif (state == STATES.MOVE_INTO_ROOM) then
             local nearestLightSource = getNearestElement(LIGHT_SOURCE_COLOR)
             nearestLightSource = nearestLightSource or { distance = 0, angle = 0}
@@ -102,7 +102,9 @@ function MoveIntoRoom.init(doorColor)
             if (targetDoor.distance > DOOR_THRESHOLD) then
                 state = STATES.INSIDE
             end
-            
+        
+        -- 3) Second threshold attained, we consider that the robot is inside
+        -- the room.
         elseif (state == STATES.INSIDE) then
             return true
         end
