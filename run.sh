02e9eaa1e4adcf1e8dc99bae9ff71d55bf6921a8
Obside <obsiagae@gmail.com>
2015-06-03 06:41:19 +0200

    run script, run 30 experiments

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-argos3 -c decision-making.xml | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//gw info.txt"
-cat info.txt output.txt > result.txt
-rm info.txt output.txt
+mkdir -p out
+
+for i in {1..30}
+do
+    echo "$i/30"
+    argos3 -c decision-making.xml | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//gw info.txt"
+    cat info.txt output.txt > out/result$i.txt
+    rm info.txt output.txt
+done
+    
